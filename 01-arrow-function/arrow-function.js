2712e27b89636e5f48161496b217364d0603ec20
Shiming <codingmylife@gmail.com>
2020-05-12 20:46:20 +0800

    init commit

---
+++ 01-arrow-function/arrow-function.js
@@ -0,0 +1,29 @@
+// 匿名函数赋值
+const squareFun1 = function (x) {
+  return x * x;
+};
+
+// 箭头函数省略function关键字
+const squareFun2 = (x) => {
+  return x * x;
+};
+
+// 如果函数体中只有一句statement，可以省略大括号
+const squareFun3 = (x) => x * x;
+
+console.log(squareFun1(2)); // output: 4
+console.log(squareFun2(2)); // output: 4
+console.log(squareFun3(2)); // output: 4
+
+const person = {
+  name: "shiming",
+  getName1: function () {
+    return this.name;
+  },
+  getName2: () => {
+    return this.name;
+  },
+};
+
+console.log(person.getName1()); // output: shiming
+console.log(person.getName2()); // output: undefined
---
+++ README.md
@@ -0,0 +1,9 @@
+## 介绍
+
+这个 repo 是用来存放一些 `Node.js` 代码示例的
+
+部分代码在片段在我的博客https://huahuayu.com有讲解
+
+## 目录
+
+01-arrow-function 箭头函数
